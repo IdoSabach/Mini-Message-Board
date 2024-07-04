ba28dbfbb8319c815b34cb345b87d374752fb115
Ido <Idosabach11@gmail.com>
2024-07-04 23:29:48 +0300

    add form file

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
 ];
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini MessageBoard', messages:messages });
-  // res.render('index', {} )
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
